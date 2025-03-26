df4d8e06fee2aa63dceb50f46068f3408e87b58a
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-03-26 17:00:54 +0100

    feat(card) : Adding card seeder & card factory WIP

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -13,7 +13,9 @@ class CardController extends Controller
      */
     public function index()
     {
-        //
+        return view('cards_list', [
+            'cards' => Card::all(),
+        ]);
     }
 
     /**
--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\CardSize;
+use App\Models\Category;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Card>
@@ -17,7 +19,13 @@ class CardFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->title(),
+            'image' => fake()->imageUrl(),
+            'music' => fake()->url(),
+            'video' => fake()->url(),
+            'description' => fake()->text(),
+            'category_id' => Category::factory(),
+            'card_size_id' => CardSize::factory(),
         ];
     }
 }
